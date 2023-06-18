dfdb9595ad9d552923d864bd539b40a64e6e4d12
Alexandre Vanier <alexandre.vanier2@gmail.com>
2023-06-18 19:42:52 +0200

    Add top bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import classes from "./App.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
@@ -6,7 +8,10 @@ function App() {
       <div className={classes.container}>
         <div className={classes.topBar}>
           <h1>Where in the world?</h1>
-          <a href="#">Dark Mode</a>
+          <a href="#">
+            <FontAwesomeIcon icon={faMoon} style={{ color: "#ffffff" }} />
+            &nbsp;&nbsp;Dark Mode
+          </a>
         </div>
       </div>
     </>
