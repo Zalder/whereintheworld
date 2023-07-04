65a2e70155b59d25f9a22fcbed95e84e71657514
Alexandre Vanier <alexandre.vanier2@gmail.com>
2023-07-04 20:57:39 +0200

    Replace axios by fetch

--- src/countriesApi.ts
+++ src/countriesApi.ts
@@ -1,28 +1,30 @@
-import axios from "axios";
 import { Country } from "./models/Country";
 
 export const getAllCountries = async (): Promise<Country[]> => {
-    try {
-        const res = await axios.get(
-          "https://restcountries.com/v3.1/all?fields=name,capital,population,flags,region,subregion,currencies,borders,cca3,languages,tld"
-        );
-        
-        return res.data
-      } catch (err) {
-        if (err instanceof Error)  throw new Response(err.message, {status: 404});
-        return [];
-      }
-}
+  try {
+    const res = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,capital,population,flags,region,subregion,currencies,borders,cca3,languages,tld"
+    );
 
-export const getCountryDetails = async (countryId: string): Promise<Country> => {
-    try {
-        const res = await axios.get(
-          `https://restcountries.com/v3.1/alpha/${countryId}`
-        );
-        
-        return res.data[0]
-      } catch (err) {
-        const error = err as Error;
-        throw new Response(error.message, {status: 404})
-      }
-}
+    return res.json();
+  } catch (err) {
+    if (err instanceof Error) throw new Response(err.message, { status: 404 });
+    return [];
+  }
+};
+
+export const getCountryDetails = async (
+  countryId: string
+): Promise<Country> => {
+  try {
+    const res = await fetch(
+      `https://restcountries.com/v3.1/alpha/${countryId}`
+    );
+    const countries = await res.json();
+
+    return countries[0];
+  } catch (err) {
+    const error = err as Error;
+    throw new Response(error.message, { status: 404 });
+  }
+};
